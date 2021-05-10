6ca4a873af173b6aab4120340c7f9be979f52286
Gennadiy Sevalnev <sevgen6613@mail.ru>
2021-05-10 17:55:50 +0700

    Codestyle fixes

--- src/functions.php
+++ src/functions.php
@@ -18,7 +18,7 @@ if (!function_exists('renderBladeTemplate')) {
      */
     function renderBladeTemplate($templateFile, $arResult, $arParams, $arLangMessages, $templateFolder, $parentTemplateFolder, $template)
     {
-        Bitrix\Main\Localization\Loc::loadMessages($_SERVER['DOCUMENT_ROOT'].$templateFolder.'/template.php');
+        Bitrix\Main\Localization\Loc::loadMessages($_SERVER['DOCUMENT_ROOT'] . $templateFolder . '/template.php');
 
         $view = BladeProvider::getViewFactory();
 
@@ -26,7 +26,7 @@ if (!function_exists('renderBladeTemplate')) {
 
         global $APPLICATION, $USER;
 
-        echo $view->file($_SERVER['DOCUMENT_ROOT'].$templateFile, compact(
+        echo $view->file($_SERVER['DOCUMENT_ROOT'] . $templateFile, compact(
             'arParams',
             'arResult',
             'arLangMessages',
@@ -37,8 +37,8 @@ if (!function_exists('renderBladeTemplate')) {
             'USER'
         ))->render();
 
-        $epilogue = $templateFolder.'/component_epilog.php';
-        if (file_exists($_SERVER['DOCUMENT_ROOT'].$epilogue)) {
+        $epilogue = $templateFolder . '/component_epilog.php';
+        if (file_exists($_SERVER['DOCUMENT_ROOT'] . $epilogue)) {
             $component = $template->__component;
             $component->SetTemplateEpilog([
                 'epilogFile'     => $epilogue,
